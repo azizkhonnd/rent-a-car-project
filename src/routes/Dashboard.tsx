058db648ba7e3f7edbca920c3fbfcad489803bda
azizkhonnd <azikklv@gmail.com>
2024-08-29 12:57:41 +0500

    bug fixes

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -7,9 +7,8 @@ const Dashboard = () => {
     return (
         <>
             <Header />
-            <div className="container flex items-center justify-center flex-col min-hd-screen">
-
-                <div className="max-w-[800px] w-full shadow-2xl p-10">
+            <div className="container flex mt-[-20px] items-center justify-center flex-col min-hd-screen">
+                <div className="max-w-[800px] w-full shadow-2xl p-8">
                     <Outlet />
                 </div>
             </div>
